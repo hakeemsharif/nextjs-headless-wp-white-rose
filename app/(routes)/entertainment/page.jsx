fa46704b249335be46399928b9451db0b035c362
Hakeem <hakeemsharif5@gmail.com>
2024-10-31 15:03:14 +0800

    White Rose 0.5h
    Adjusted data

--- app/(routes)/entertainment/page.jsx
+++ app/(routes)/entertainment/page.jsx
@@ -25,14 +25,13 @@ export default async function Entertainment() {
     throw new Error("Failed to fetch data");
   }
 
-  const data = await res.json();
-  
   for (const post of data) {
     const imageUrl = post._embedded["wp:featuredmedia"][0].source_url;
     post.blurDataURL = await getBase64(imageUrl);
   }
-  
 
+  const data = await res.json();
+  
   return (
     <section className="group-section">
       <title>White Rose | Artists</title>
